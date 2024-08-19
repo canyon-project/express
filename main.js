6bd9bca07c0c91f31c3fbc322f0c061ef1e52ac4
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-08-19 14:48:22 +0800

    upd

--- main.js
+++ main.js
@@ -6,11 +6,17 @@ import {createProxyMiddleware} from "http-proxy-middleware";
 
 const app = express();
 
-// app.use(express.static('public'));
+const redirectApp = express(); // 创建一个新的 Express 应用用于重定向
+
+// 使用 HTTP 服务器处理重定向
+redirectApp.use((req, res) => {
+    const httpsUrl = `https://${req.headers.host}${req.url}`;
+    res.redirect(301, httpsUrl); // 301 是永久重定向
+});
 
 app.use('/', (req, res, next) => {
     if (req.headers.host.includes('www.canyonalls.com')) {
-        return res.redirect('https://canyonalls.com' + req.url);
+        return res.redirect(301,'https://canyonalls.com' + req.url);
     }
     // Use the proxy middleware
     createProxyMiddleware({
@@ -30,4 +36,9 @@ const options = {
 // 创建HTTPS服务器
 https.createServer(options, app).listen(443, () => {
     console.log('HTTPS server running on port 443');
+});
+
+// 创建 HTTP 服务器并重定向到 HTTPS
+http.createServer(redirectApp).listen(80, () => {
+    console.log('HTTP server running on port 80 and redirecting to HTTPS');
 });
